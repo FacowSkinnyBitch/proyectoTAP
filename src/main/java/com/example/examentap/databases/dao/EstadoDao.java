a75235f193ee9628764a9c26e64777d06f5c17f5
JR <ramirezsillerojesusmanuel158@gmail.com>
2024-11-30 23:06:29 -0600

    Agregado-Ciudad/CiudadDao/EstadoDao (El dao no es necesario pero esta por si se ocupa)

--- src/main/java/com/example/examentap/databases/dao/EstadoDao.java
+++ src/main/java/com/example/examentap/databases/dao/EstadoDao.java
@@ -1,4 +1,73 @@
 package com.example.examentap.databases.dao;
 
-public class EstadoDao {
+import com.example.examentap.databases.MySQLConnection;
+import com.example.examentap.models.Estado;
+import javafx.collections.FXCollections;
+
+import java.sql.*;
+import java.util.List;
+import java.util.Optional;
+
+public class EstadoDao extends MySQLConnection implements Dao<Estado> {
+    Connection conn = getConnection();
+
+
+    @Override
+    public Optional<Estado> findById(int id) {
+        Optional<Estado> optionalEstado = Optional.empty();
+        String query = "SELECT * FROM estado WHERE id_estado = ?";
+        try{
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setLong(1, id);
+            ResultSet rs = statement.executeQuery();
+
+            if (rs.next()) {
+                Estado edo = new Estado();
+                edo.setEstado(rs.getString("estado"));
+                edo.setId_estado(rs.getInt("id_estado"));
+                optionalEstado = Optional.of(edo);
+            }
+
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return optionalEstado;
+    }
+
+    @Override
+    public List<Estado> findAll() {
+        List<Estado> estadoList = FXCollections.observableArrayList();
+        String query = "SELECT edo.estado, edo.id_estado " +
+                "FROM estado edo";
+
+        try{
+            PreparedStatement statement = conn.prepareStatement(query);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                Estado edo = new Estado();
+                edo.setEstado(rs.getString("estado"));
+                edo.setId_estado(rs.getInt("id_estado"));
+                estadoList.add(edo);
+            }
+
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return List.of();
+    }
+
+    @Override
+    public boolean save(Estado record) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Estado record) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
+    }
 }
--- src/main/java/com/example/examentap/models/Ciudad.java
+++ src/main/java/com/example/examentap/models/Ciudad.java
@@ -3,13 +3,13 @@ package com.example.examentap.models;
 public class Ciudad {
     private String ciudad;
     private int id_ciudad;
-    private int id_estado;
+    private String estado;
 
     public Ciudad() {}
-    public Ciudad(String ciudad, int id_Ciudad, int id_estado) {
+    public Ciudad(String ciudad, int id_Ciudad, String estado) {
         this.ciudad = ciudad;
         this.id_ciudad = id_Ciudad;
-        this.id_estado = id_estado;
+        this.estado = estado;
     }
 
     public String getCiudad() {
@@ -28,11 +28,11 @@ public class Ciudad {
         this.id_ciudad = id_Ciudad;
     }
 
-    public int getId_estado() {
-        return id_estado;
+    public String getEstado() {
+        return estado;
     }
 
-    public void setId_estado(int id_estado) {
-        this.id_estado = id_estado;
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
 }
