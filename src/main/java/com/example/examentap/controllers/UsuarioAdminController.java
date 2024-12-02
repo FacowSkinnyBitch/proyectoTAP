d354b7fba46029215584cfcce1dc5e91c095e41a
Emmy la Bola <or715.22.a.s@gmail.com>
2024-12-02 10:37:40 -0600

    cambios_usuarioAdminController

--- src/main/java/com/example/examentap/controllers/UsuarioAdminController.java
+++ src/main/java/com/example/examentap/controllers/UsuarioAdminController.java
@@ -52,12 +52,27 @@ public class UsuarioAdminController implements Initializable {
     public static final String DEST3 = "results/excel/Usuarios.xlsx";
 
     ContextMenu contextMenu = new ContextMenu();
-    MenuItem menuItemSelectUser = new MenuItem("Info User");
+    //MenuItem menuItemSelectUser = new MenuItem("Info User");
     MenuItem menuItemDeleteUser = new MenuItem("Delete");
     MenuItem menuItemUpdate = new MenuItem("Update");
 
     private List<Usuario> usuarioList = new ArrayList<>();
     private UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        cargarTablaUsuario();
+        initContextMenu();
+        initMetodosCrud();
+
+        tvUsuarios.setOnMouseClicked(mouseEvent ->{
+            Usuario selectedUser = (Usuario) tvUsuarios.getSelectionModel().getSelectedItem();
+            if (mouseEvent.getClickCount() == 2) {
+                onReadUser(selectedUser);
+            }
+        });
+
+    }
 
     @FXML
     private void onReturn(ActionEvent event) throws IOException {
@@ -77,25 +92,97 @@ public class UsuarioAdminController implements Initializable {
         }
     }
 
-    private void metodoCRUD(){
-        menuItemDeleteUser.setOnAction(event -> {
-            if(confirmDelete()){
+    private void initContextMenu(){
+        /*FontIcon iconDelete = new FontIcon();
+        iconDelete.setIconLiteral("anto-close-circle");
+        iconDelete.setIconSize(20);
+        iconDelete.setIconColor(Color.RED);
+        menuItemSelectUser.setGraphic(iconDelete);*/
 
+
+        FontIcon iconComplete = new FontIcon();
+        iconComplete.setIconLiteral("anto-file-done");
+        iconComplete.setIconSize(20);
+        iconComplete.setIconColor(Color.GREEN);
+        menuItemUpdate.setGraphic(iconComplete);
+
+        FontIcon iconIncomplete = new FontIcon();
+        iconIncomplete.setIconLiteral("antf-frown");
+        iconIncomplete.setIconSize(20);
+        iconIncomplete.setIconColor(Color.DARKMAGENTA);
+        contextMenu.getItems().addAll(menuItemUpdate,menuItemDeleteUser);
+        menuItemDeleteUser.setGraphic(iconIncomplete);
+    }
+    private void cargarTablaUsuario() {
+        usuarioDAO.findAll();
+        tvid.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tvusuario.setCellValueFactory(new PropertyValueFactory<>("user"));
+        tvnombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        tvprimer_apellido.setCellValueFactory(new PropertyValueFactory<>("primer_apellido"));
+        tvsegundo_apellido.setCellValueFactory(new PropertyValueFactory<>("segundo_apellido"));
+        tvemail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        tvcontraseya.setCellValueFactory(new PropertyValueFactory<>("contraseya"));
+        tvdireccion.setCellValueFactory(new PropertyValueFactory<>("direccion"));
+        tvgenero.setCellValueFactory(new PropertyValueFactory<>("genero"));
+        tvnacimiento.setCellValueFactory(new PropertyValueFactory<>("nacimiento"));
+        tvrole.setCellValueFactory(new PropertyValueFactory<>("role"));
+
+        usuarioList = usuarioDAO.findAll();
+        tvUsuarios.setItems(FXCollections.observableArrayList(usuarioList));
+    }
+
+    private void initMetodosCrud(){
+
+        menuItemDeleteUser.setOnAction(event -> {
+        Usuario selectedUser = (Usuario) tvUsuarios.getSelectionModel().getSelectedItem();
+            if(selectedUser != null){
+                if(confirmDelete()){
+                    System.out.println("id deleted: "+selectedUser.getId());
+                    onDeleteUser(selectedUser);
+                    //reloadAll();
+                    //borrar los usuarios que se encuentren en la tabla de datos_cita
+                }
             }
         });
-        menuItemSelectUser.setOnAction(event -> {
-            onReadUser();
-            //usuarioDAO.update(tvUsuarios.getSelectionModel().getSelectedItem().getId(), true);
-        });
+
         menuItemUpdate.setOnAction(event -> {
-            onUpdateUser();
+            Usuario selectedUser = (Usuario) tvUsuarios.getSelectionModel().getSelectedItem();
+            onUpdateUser(selectedUser);
         });
         btn_crearNuevoUsuario.setOnAction(event -> {
             onCreateUser();
         });
 
         tvUsuarios.setContextMenu(contextMenu);
+    }
+    private boolean confirmDelete(){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirm Delete");
+        alert.setContentText("Are you sure you want to delete this user?");
+        Optional<ButtonType> result = alert.showAndWait();
+        return (result.get() == ButtonType.OK);
+    }
 
+
+    @FXML
+    private void onCreateUser() {
+        // Crear un nuevo objeto Usuario vacío
+        Usuario nuevoUsuario = new Usuario();
+
+        // Crear un diálogo de entrada para capturar los datos del usuario
+        Dialog<Usuario> dialog = crearDialogoCreacion(nuevoUsuario);
+        Optional<Usuario> resultado = dialog.showAndWait();
+
+        resultado.ifPresent(usuarioCreado -> {
+            if (usuarioDAO.save(usuarioCreado)) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, "Usuario creado correctamente.", ButtonType.OK);
+                alert.showAndWait();
+                cargarTablaUsuario();  // Recargar la tabla de usuarios
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "No se pudo crear el usuario.", ButtonType.OK);
+                alert.showAndWait();
+            }
+        });
     }
     @FXML
     private void onGenerarPdf() throws IOException {
@@ -140,113 +227,22 @@ public class UsuarioAdminController implements Initializable {
         return (result.get() == ButtonType.OK);
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        cargarTablaUsuario();
-        metodoCRUD();
-        initContextMenu();
-        initMetodosCrud();
-    }
-    private void initContextMenu(){
-        FontIcon iconDelete = new FontIcon();
-        iconDelete.setIconLiteral("anto-close-circle");
-        iconDelete.setIconSize(20);
-        iconDelete.setIconColor(Color.RED);
-        menuItemSelectUser.setGraphic(iconDelete);
-
-
-        FontIcon iconComplete = new FontIcon();
-        iconComplete.setIconLiteral("anto-file-done");
-        iconComplete.setIconSize(20);
-        iconComplete.setIconColor(Color.GREEN);
-        menuItemUpdate.setGraphic(iconComplete);
-
-        FontIcon iconIncomplete = new FontIcon();
-        iconIncomplete.setIconLiteral("antf-frown");
-        iconIncomplete.setIconSize(20);
-        iconIncomplete.setIconColor(Color.DARKMAGENTA);
-        contextMenu.getItems().addAll(menuItemSelectUser,menuItemUpdate,menuItemDeleteUser);
-        menuItemDeleteUser.setGraphic(iconIncomplete);
-    }
-    private void cargarTablaUsuario() {
-        usuarioDAO.findAll();
-        tvid.setCellValueFactory(new PropertyValueFactory<>("id"));
-        tvusuario.setCellValueFactory(new PropertyValueFactory<>("user"));
-        tvnombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
-        tvprimer_apellido.setCellValueFactory(new PropertyValueFactory<>("primer_apellido"));
-        tvsegundo_apellido.setCellValueFactory(new PropertyValueFactory<>("segundo_apellido"));
-        tvemail.setCellValueFactory(new PropertyValueFactory<>("email"));
-        tvcontraseya.setCellValueFactory(new PropertyValueFactory<>("contraseya"));
-        tvdireccion.setCellValueFactory(new PropertyValueFactory<>("direccion"));
-        tvgenero.setCellValueFactory(new PropertyValueFactory<>("genero"));
-        tvnacimiento.setCellValueFactory(new PropertyValueFactory<>("nacimiento"));
-        tvrole.setCellValueFactory(new PropertyValueFactory<>("role"));
-
-        usuarioList = usuarioDAO.findAll();
-        tvUsuarios.setItems(FXCollections.observableArrayList(usuarioList));
-    }
-
-    private void initMetodosCrud(){
-        menuItemDeleteUser.setOnAction(event -> {
-            Usuario usuarioSeleccionado = (Usuario) tvUsuarios.getSelectionModel().getSelectedItem();
-            if(usuarioSeleccionado != null){
-                if(confirmDelete()){
-                    System.out.println("id deleted: "+usuarioSeleccionado.getId());
-                    onDeleteUser();
-                    //reloadAll();
-                    //borrar los usuarios que se encuentren en la tabla de datos_cita
-                }
-            }
-        });
-    }
-    private boolean confirmDelete(){
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Confirm Delete");
-        alert.setContentText("Are you sure you want to delete this user?");
-        Optional<ButtonType> result = alert.showAndWait();
-        return (result.get() == ButtonType.OK);
-    }
-
-
     @FXML
-    private void onCreateUser() {
-        // Crear un nuevo objeto Usuario vacío
-        Usuario nuevoUsuario = new Usuario();
-
-        // Crear un diálogo de entrada para capturar los datos del usuario
-        Dialog<Usuario> dialog = crearDialogoCreacion(nuevoUsuario);
-        Optional<Usuario> resultado = dialog.showAndWait();
-
-        resultado.ifPresent(usuarioCreado -> {
-            if (usuarioDAO.save(usuarioCreado)) {
-                Alert alert = new Alert(Alert.AlertType.INFORMATION, "Usuario creado correctamente.", ButtonType.OK);
-                alert.showAndWait();
-                cargarTablaUsuario();  // Recargar la tabla de usuarios
-            } else {
-                Alert alert = new Alert(Alert.AlertType.ERROR, "No se pudo crear el usuario.", ButtonType.OK);
-                alert.showAndWait();
-            }
-        });
-    }
-
-
-    @FXML
-    private void onReadUser() {
-        Usuario usuarioSeleccionado = (Usuario) tvUsuarios.getSelectionModel().getSelectedItem();
-        if (usuarioSeleccionado != null) {
+    private void onReadUser(Usuario selectedUser) {
+        if (selectedUser != null) {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Detalles del Usuario");
             alert.setHeaderText("Información detallada del usuario");
             alert.setContentText(
-                    "ID: " + usuarioSeleccionado.getId() + "\n" +
-                            "Usuario: " + usuarioSeleccionado.getUser() + "\n" +
-                            "Nombre: " + usuarioSeleccionado.getNombre() + " " + usuarioSeleccionado.getPrimer_apellido() + " " + usuarioSeleccionado.getSegundo_apellido() + "\n" +
-                            "Email: " + usuarioSeleccionado.getEmail() + "\n" +
-                            "Contraseña: " + usuarioSeleccionado.getContraseya() + "\n" +
-                            "Telefono: " + usuarioSeleccionado.getTelefono() + "\n" +
-                            "Genero: " + usuarioSeleccionado.getGenero() + "\n" +
-                            "Fecha de Nacimiento: " + usuarioSeleccionado.getNacimiento() + "\n" +
-                            "Rol: " + usuarioSeleccionado.getRole() + "\n"
+                    "ID: " + selectedUser.getId() + "\n" +
+                            "Usuario: " + selectedUser.getUser() + "\n" +
+                            "Nombre: " + selectedUser.getNombre() + " " + selectedUser.getPrimer_apellido() + " " + selectedUser.getSegundo_apellido() + "\n" +
+                            "Email: " + selectedUser.getEmail() + "\n" +
+                            "Contraseña: " + selectedUser.getContraseya() + "\n" +
+                            "Telefono: " + selectedUser.getTelefono() + "\n" +
+                            "Genero: " + selectedUser.getGenero() + "\n" +
+                            "Fecha de Nacimiento: " + selectedUser.getNacimiento() + "\n" +
+                            "Rol: " + selectedUser.getRole() + "\n"
             );
             alert.showAndWait();
         } else {
@@ -256,10 +252,10 @@ public class UsuarioAdminController implements Initializable {
     }
 
     @FXML
-    private void onUpdateUser() {
-        Usuario usuarioSeleccionado = (Usuario) tvUsuarios.getSelectionModel().getSelectedItem();
-        if (usuarioSeleccionado != null) {
-            Dialog<Usuario> dialog = crearDialogoEdicion(usuarioSeleccionado);
+    private void onUpdateUser(Usuario selectedUser) {
+
+        if (selectedUser != null) {
+            Dialog<Usuario> dialog = crearDialogoEdicion(selectedUser);
             Optional<Usuario> resultado = dialog.showAndWait();
 
             resultado.ifPresent(usuarioEditado -> {
@@ -280,19 +276,19 @@ public class UsuarioAdminController implements Initializable {
 
 
     @FXML
-    private void onDeleteUser() {
-        Usuario usuarioSeleccionado = (Usuario) tvUsuarios.getSelectionModel().getSelectedItem();
-        if (usuarioSeleccionado != null) {
-            if (confirmDelete()) {
-                if (usuarioDAO.delete(usuarioSeleccionado.getId())) {
-                    Alert alert = new Alert(Alert.AlertType.INFORMATION, "Usuario eliminado correctamente.", ButtonType.OK);
-                    alert.showAndWait();
-                    cargarTablaUsuario();
-                } else {
-                    Alert alert = new Alert(Alert.AlertType.ERROR, "No se pudo eliminar el usuario.", ButtonType.OK);
-                    alert.showAndWait();
-                }
+    private void onDeleteUser(Usuario selectedUser) {
+
+        if (selectedUser != null) {
+
+            if (usuarioDAO.delete(selectedUser.getId())) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, "Usuario eliminado correctamente.", ButtonType.OK);
+                alert.showAndWait();
+                cargarTablaUsuario();
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "No se pudo eliminar el usuario.", ButtonType.OK);
+                alert.showAndWait();
             }
+
         } else {
             Alert alert = new Alert(Alert.AlertType.WARNING, "Seleccione un usuario para eliminar.", ButtonType.OK);
             alert.showAndWait();
